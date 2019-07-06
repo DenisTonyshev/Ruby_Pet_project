27446f959ef382136f13a4619e3548eec33323e0
DenisTonyshev <tonyshevd@gmail.com>
2019-07-06 21:03:57 +0300

    First steps Ruby

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+
+  root "home#about"
+  get "info" , to: 'home#info'
+
+  resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
