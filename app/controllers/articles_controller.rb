a279239c6f777d8d8c8bb57b56d0fe74ba92f9f9
DenisTonyshev <tonyshevd@gmail.com>
2019-07-13 18:46:48 +0300

    Styled Errors, and MSG partials ToDO Clen Code(DRY)

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,7 +5,49 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    render plain: params[:article].inspect
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:success] = "Article was successfully created"
+      redirect_to article_path(@article)
+    else
+
+      render 'new'
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:success] = "Article was updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def destroy
+    @article=Article.find(params[:id])
+    @article.destroy
+    flash[:danger] = "Article successfully deleted"
+    redirect_to articles_path
+  end
+
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :description)
   end
 
 end
